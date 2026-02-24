d748f9fc273480d432cb245c0f7d0574d7512566
Prathik Shetty <prathikshetty006@gmail.com>
2026-02-24 15:52:58 +0530

    feat(users): add user interface model

---
+++ backend/src/users/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+import { Role } from '../enums/role.enum';
+
+export interface User {
+  id: string;
+  email: string;
+  passwordHash: string;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
