2c2116f6385297de5b533a4a81437509f566eba0
Prathik Shetty <prathikshetty006@gmail.com>
2026-02-26 15:23:14 +0530

    feat(config): add jwt configuration module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import jwtConfig from './config/jwt.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [jwtConfig],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
